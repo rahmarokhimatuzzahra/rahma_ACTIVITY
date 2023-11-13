665415347cb06ded1139051bdc9c71c3c27ae20f
uswtnnisa <uswatunnisa382@gmail.com>
2023-11-13 11:30:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rahma_activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.rahma_activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val Intent = Intent(  this, SecondActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rahma_activity/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.rahma_activity;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
